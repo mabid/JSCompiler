a6f7d1bdd44111afe9d6fd09a067c0ad6df463bb
Abid Mahmood <mabid.mahmood@gmail.com>
2012-04-17 00:39:57 +0500

    tests

--- mus/compile.js
+++ mus/compile.js
@@ -6,11 +6,11 @@ var convert_pitch = function(pitch){
 }
 
 var note_obj = function(expr, start){
-  return { tag:'note',dur:expr.dur,pitch:convert_pitch(expr.pitch),start:start};
+	return { tag:'note',dur:expr.dur,pitch:convert_pitch(expr.pitch),start:start};
 };
 
 var rest_obj = function(expr, start){
-  return { tag:'rest',dur:expr.dur,start:start};
+	return { tag:'rest',dur:expr.dur,start:start};
 };
 
 var process_note = function(note, start){
@@ -36,47 +36,47 @@ var compile = function(musexpr){
 		return {endTime : compiled.endTime, value : repeats};
 	};
 
-  var rec_compile = function(expr){
-    switch(expr.tag){
+	var rec_compile = function(expr){
+		switch(expr.tag){
 			case 'rest':
 				return process_rest(expr, start);
-      case 'note':
+			case 'note':
 				return process_note(expr, start);
-      case 'repeat':
+			case 'repeat':
 				return process_repeat(expr, start);
-      default :
-        var l_part = rec_compile(expr.left);
-        if(expr.tag == 'seq'){
-          start = l_part.endTime;
-        }
-        var r_part = rec_compile(expr.right);
-        var e_time = (l_part.endTime > r_part.endTime) ? l_part.endTime : r_part.endTime;
-        return {endTime:e_time, value : l_part.value.concat(r_part.value) };
-    }
-  };
+			default :
+				var l_part = rec_compile(expr.left);
+				if(expr.tag == 'seq'){
+					start = l_part.endTime;
+				}
+				var r_part = rec_compile(expr.right);
+				var e_time = (l_part.endTime > r_part.endTime) ? l_part.endTime : r_part.endTime;
+				return {endTime:e_time, value : l_part.value.concat(r_part.value) };
+		}
+	};
 
 
-  var start = 0;
-  return rec_compile(musexpr).value;
+	var start = 0;
+	return rec_compile(musexpr).value;
 };
 
 var melody_mus = 
-    { tag: 'seq',
-      left:
-       { tag: 'repeat',
-         section:
-          { tag: 'seq',
-            left: { tag: 'note', pitch: 'a4', dur: 250 },
-            right: { tag: 'rest', dur: 250 } },
-          count: 3 },
-      right:
-       { tag: 'par',
-         left: { tag: 'note', pitch: 'c4', dur: 500 },
-         right:
-         { tag: 'seq',
-           left: { tag: 'note', pitch: 'd4', dur: 500 },
-           right: { tag: 'note', pitch: 'f3', dur: 250 } } } };
+{ tag: 'seq',
+left:
+	{ tag: 'repeat',
+section:
+		{ tag: 'seq',
+left: { tag: 'note', pitch: 'a4', dur: 250 },
+			right: { tag: 'rest', dur: 250 } },
+		count: 3 },
+	right:
+	{ tag: 'par',
+left: { tag: 'note', pitch: 'c4', dur: 500 },
+			right:
+			{ tag: 'seq',
+left: { tag: 'note', pitch: 'd4', dur: 500 },
+			right: { tag: 'note', pitch: 'f3', dur: 250 } } } };
 
-//console.log(melody_mus);
+console.log(melody_mus);
 console.log(compile(melody_mus));
 
