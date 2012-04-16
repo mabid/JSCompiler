4c8b60f6ff051f3f9e79bf49656637ccc41ad75c
Abid Mahmood <mabid.mahmood@gmail.com>
2012-04-16 23:46:15 +0500

    some basic tests

--- mus/compile.js
+++ mus/compile.js
@@ -37,3 +37,18 @@ var compile = function(musexpr){
   var start = 0;
   return rec_compile(musexpr).value;
 };
+
+
+var melody_mus = 
+    { tag: 'seq',
+      left: 
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'a4', dur: 250 },
+         right: { tag: 'note', pitch: 'b4', dur: 250 } },
+      right:
+       { tag: 'seq',
+         left: { tag: 'note', pitch: 'c4', dur: 500 },
+         right: { tag: 'note', pitch: 'd4', dur: 500 } } };
+
+console.log(melody_mus);
+console.log(compile(melody_mus));
